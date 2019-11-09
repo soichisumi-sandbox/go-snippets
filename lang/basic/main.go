bf0e364f17ab184feb2ee06bbc5d7237801fa6ec
Soichi Sumi <soichi.sumi@gmail.com>
2019-11-09 20:50:03 +0900

    update basic

--- lang/basic/main.go
+++ lang/basic/main.go
@@ -1,1 +1,33 @@
-package basic
+package main
+
+import "fmt"
+
+
+func manipulateByPointer(){
+	f := func(v *uint32){
+		*v += 5
+	}
+
+	var x uint32 = 5
+	fmt.Printf("before x: %d\n", x)
+
+	f(&x)
+
+	fmt.Printf("after x: %d\n", x)
+}
+
+// Range で生成される変数は for内で使い回される
+// このため、rangeで生成される変数をキャプチャしてはいけない
+func valuesOfRangeSyntax(){
+	s := []string{"a", "b", "c", "d"}
+	for i, v := range s {
+		fmt.Printf("loop %d\n", i)
+		fmt.Printf("i: %d, v: %s\n", i, v)
+		fmt.Printf("address i: %+v, v: %+v\n", &i, &v)
+	}
+}
+
+func main(){
+	//manipulateByPointer()
+	valuesOfRangeSyntax()
+}

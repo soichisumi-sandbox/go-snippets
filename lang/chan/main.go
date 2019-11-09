612e3b0341a0b24c6007beef90d0f8b79dfeeb31
Soichi Sumi <soichi.sumi@gmail.com>
2019-11-09 20:49:54 +0900

    update chan

--- lang/chan/main.go
+++ lang/chan/main.go
@@ -1,1 +1,19 @@
-package _chan
+package main
+
+import "fmt"
+
+func channelBasic() {
+	// closeFirst
+	c := make(chan int, 4)
+	for _, v := range []int{ 1, 4, 25, 52 } {
+		c <- v
+	}
+	close(c)
+	for v := range c {
+		fmt.Printf("%d received\n", v)
+	}
+}
+
+func main(){
+	channelBasic()
+}
